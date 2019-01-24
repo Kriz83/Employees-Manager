9249c8e13c5ea4abdb90f5eeeff57640ff19ed5d
Kriz83 <krzysztofkorman83@gmail.com>
2019-01-24 14:02:46 +0100

    Add edit option to Position

--- src/Controller/AddDataController.php
+++ src/Controller/AddDataController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Position;
 use App\Form\Position\AddPositionType;
+use App\Form\Position\EditPositionType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -48,6 +49,33 @@ class AddDataController extends AbstractController
         }
 
         return $this->render('data/position/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/data/position/edit/{positionId}", name="app_data_positions_edit")
+     */
+    public function editPosition(Request $request, EntityManagerInterface $em, $positionId)
+    {       
+        //get positions array
+        $position = $em
+            ->getRepository('App:Position')
+            ->findOneById($positionId);
+
+        $form = $this->createForm(EditPositionType::class, $position);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($position);
+            $em->flush();
+
+            return $this->redirectToRoute('app_data_positions_show_all', array(
+            ));
+        }
+
+        return $this->render('data/position/edit.html.twig', [
             'form' => $form->createView(),
         ]);
     }
